48bf2aed5d23cd8235b432d0b41b470273ce3310
danfeng <danfengluo_math@163.com>
2022-09-14 15:15:57 +0800

    Provide data.

---
+++ DifferentArithmetic/rainbow/javafile/editandrun.java
@@ -0,0 +1,203 @@
+// STAR-CCM+ macro: edit.java
+// Written by STAR-CCM+ 14.02.012
+package macro;
+
+import java.util.*;
+import star.common.*;
+import star.base.neo.*;
+import star.energy.*;
+import star.flow.*;
+import star.species.*;
+import star.base.report.*;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+
+@SuppressWarnings("unchecked")
+public class editandrun extends StarMacro {
+
+  private void execute0() {
+
+    Simulation simulation_0 = 
+      getActiveSimulation();
+
+    Region region_0 = simulation_0.getRegionManager().getRegion("roomex");
+
+    Boundary boundary_1 = region_0.getBoundaryManager().getBoundary("InletRHa");
+
+    MassFractionProfile massFractionProfile_1 = boundary_1.getValues().get(MassFractionProfile.class);
+    // 33
+	massFractionProfile_1.getMethod(ConstantArrayProfileMethod.class).getQuantity().setArray(new DoubleVector(new double[] {0.00988,0.99012}));
+
+	VelocityMagnitudeProfile velocityMagnitudeProfile_1 = boundary_1.getValues().get(VelocityMagnitudeProfile.class);
+    // 37
+	velocityMagnitudeProfile_1.getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(0);
+	
+	
+	
+    Boundary boundary_2 = region_0.getBoundaryManager().getBoundary("InletRHb");
+
+    MassFractionProfile massFractionProfile_2 = boundary_2.getValues().get(MassFractionProfile.class);
+    // 45
+	massFractionProfile_2.getMethod(ConstantArrayProfileMethod.class).getQuantity().setArray(new DoubleVector(new double[] {0.00988,0.99012}));
+
+    VelocityMagnitudeProfile velocityMagnitudeProfile_2 = boundary_2.getValues().get(VelocityMagnitudeProfile.class);
+    // 49
+	velocityMagnitudeProfile_2.getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(0);
+
+
+
+    Boundary boundary_3 = region_0.getBoundaryManager().getBoundary("InletRHc");
+
+    MassFractionProfile massFractionProfile_3 = boundary_3.getValues().get(MassFractionProfile.class);
+    // 57
+	massFractionProfile_3.getMethod(ConstantArrayProfileMethod.class).getQuantity().setArray(new DoubleVector(new double[] {0.00988,0.99012}));
+
+    VelocityMagnitudeProfile velocityMagnitudeProfile_3 = boundary_3.getValues().get(VelocityMagnitudeProfile.class);
+    // 61
+	velocityMagnitudeProfile_3.getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(0);
+
+
+    // 是否要加indoor风口
+    Boundary boundary_0 =
+      region_0.getBoundaryManager().getBoundary("InletDoor");
+    // 68
+    InletBoundary inletBoundary_0 = ((InletBoundary) simulation_0.get(ConditionTypeManager.class).get(InletBoundary.class));
+    // 70
+    boundary_0.setBoundaryType(inletBoundary_0);
+    // 72--------------------------------------------
+    StaticTemperatureProfile staticTemperatureProfile_0 = boundary_0.getValues().get(StaticTemperatureProfile.class);
+
+    staticTemperatureProfile_0.getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(24.0);
+
+    MassFractionProfile massFractionProfile_0 = boundary_0.getValues().get(MassFractionProfile.class);
+
+    massFractionProfile_0.getMethod(ConstantArrayProfileMethod.class).getQuantity().setArray(new DoubleVector(new double[] {0.0097801, 0.9902197}));
+
+    VelocityMagnitudeProfile velocityMagnitudeProfile_0 = boundary_0.getValues().get(VelocityMagnitudeProfile.class);
+
+    velocityMagnitudeProfile_0.getMethod(ConstantScalarProfileMethod.class).getQuantity().setValue(2.0);
+    // 82-----------------------------------------------
+
+
+    simulation_0.getSimulationIterator().runAutomation();
+
+    ReportMonitor reportMonitor_0 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B901RH Monitor"));
+
+    ReportMonitor reportMonitor_1 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B902RH Monitor"));
+
+    ReportMonitor reportMonitor_2 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B903RH Monitor"));
+
+    ReportMonitor reportMonitor_3 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B904RH Monitor"));
+
+    ReportMonitor reportMonitor_4 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B905RH Monitor"));
+
+    ReportMonitor reportMonitor_5 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B906RH Monitor"));
+
+    ReportMonitor reportMonitor_6 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B907RH Monitor"));
+
+    ReportMonitor reportMonitor_7 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B908RH Monitor"));
+
+    ReportMonitor reportMonitor_8 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B909RH Monitor"));
+
+    ReportMonitor reportMonitor_9 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B910RH Monitor"));
+
+    ReportMonitor reportMonitor_10 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B911RH Monitor"));
+
+    ReportMonitor reportMonitor_11 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B912RH Monitor"));
+
+    ReportMonitor reportMonitor_12 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B913RH Monitor"));
+
+    ReportMonitor reportMonitor_13 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B914RH Monitor"));
+
+    ReportMonitor reportMonitor_14 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B915RH Monitor"));
+
+    ReportMonitor reportMonitor_15 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B916RH Monitor"));
+
+    ReportMonitor reportMonitor_16 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B917RH Monitor"));
+
+    ReportMonitor reportMonitor_17 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B918RH Monitor"));
+
+    ReportMonitor reportMonitor_18 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B919RH Monitor"));
+
+    ReportMonitor reportMonitor_19 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B920RH Monitor"));
+
+    ReportMonitor reportMonitor_20 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("\u70B921RH Monitor"));
+
+    ReportMonitor reportMonitor_21 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("InletDoor Monitor"));
+
+    ReportMonitor reportMonitor_22 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("InletRHa Monitor"));
+
+    ReportMonitor reportMonitor_23 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("InletRHb Monitor"));
+
+    ReportMonitor reportMonitor_24 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("InletRHc Monitor"));
+
+    ReportMonitor reportMonitor_25 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("InletVela Monitor"));
+
+    ReportMonitor reportMonitor_26 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("InletVelb Monitor"));
+
+    ReportMonitor reportMonitor_27 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("InletVelc Monitor"));
+
+    ReportMonitor reportMonitor_28 =
+      ((ReportMonitor) simulation_0.getMonitorManager().getMonitor("InletVelDoor Monitor"));
+
+    simulation_0.getMonitorManager().export("RHfile.csv", ",", new NeoObjectVector(new Object[] {
+    reportMonitor_0, reportMonitor_1, reportMonitor_2, reportMonitor_3, reportMonitor_4, reportMonitor_5,
+    reportMonitor_6, reportMonitor_7, reportMonitor_8, reportMonitor_9, reportMonitor_10, reportMonitor_11,
+    reportMonitor_12, reportMonitor_13, reportMonitor_14, reportMonitor_15, reportMonitor_16, reportMonitor_17,
+    reportMonitor_18, reportMonitor_19, reportMonitor_20, reportMonitor_21, reportMonitor_22, reportMonitor_23,
+    reportMonitor_24, reportMonitor_25, reportMonitor_26, reportMonitor_27, reportMonitor_28}));
+
+    simulation_0.saveState("humidity.sim");
+
+    try {
+        /* 写入Txt文件 */
+        File writename = new File("javafile/info.txt"); // 相对路径，如果没有则要建立一个新的output。txt文件
+        writename.createNewFile(); // 创建新文件
+        BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+        out.write("false");
+        out.flush(); // 把缓存区内容压入文件
+        out.close(); // 最后记得关闭文件
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+  }
+
+  public void execute() {
+    execute0();
+  }
+
+
+}
--- readme.md
+++ readme.md
@@ -53,28 +53,89 @@ in each folder.
 
 ## File Directory ##
 
-HumidityControl
+- HumidityControl
+  + DifferentArithmetic
+    > Model training of DQN, Rainbow, and Rainbow ablation algorithms
+  + DifferentSetpoints
+    > Experiments at different monitoring points (6, 9 and 15 humidity sensors) of model A.
+  + DifferentTime
+    > Experiment of different reporting time intervals (1, 3 and 5 minutes) of model A.
+  + modelAtest
+    > Test the effect of different algorithms in model A.
+  + modelB
+    > Test the effect of different algorithms in model B
+  + pid
+    > PID algorithm.
+  + solutions
+    > The module related to reinforcement learning and the module interacting with the model of STAR-CCM+.
+  
+## Folder Details  ##
 
--- DifferentArithmetic
-+ Model training of DQN, Rainbow, and Rainbow ablation algorithms
+Only the data and file descriptions in the directory of the rainbow algorithm are described here. 
+The file storage methods of other algorithms are the same.
 
--- DifferentSetpoints
-+ Experiments at different monitoring points (6, 9 and 15 humidity sensors) of model A.
+The specific location of the rainbow directory:
 
--- DifferentTime
-+ Experiment of different reporting time intervals (1, 3 and 5 minutes) of model A.
+- HumidityControl
+  + DifferentArithmetic
+    + rainbow
 
--- modelAtest
-+ Test the effect of different algorithms in model A.
+The description under the rainbow folder:
 
--- modelB
-+ Test the effect of different algorithms in model B
+- javafile 
+  > Store simulation models and macro commands.
+  + checkinfo.java 
+    > Check the info.txt file operation.
+  + editandrun.java 
+    > Modify the boundary conditions related to the CFD model, such as the temperature, 
+    humidity and wind speed of the constant humidity machine.
+  + info.txt 
+    > The time verification file maintains the physical time consistency between the simulation model and the agent.
+  + reset.java 
+    > Reset the macro command of the simulation environment.
+  + RHfile.csv 
+    > Interactive data of the current episode.
+  + humidity.sim 
+    > CFD simulation model.
+- out 
+  > Storing history files (model data and agent strategy)
+  + csvfile 
+    >Record the interactive data of each episode.
+  + logs 
+    >Record the output of the console.
+  + savemodel 
+    >Target network for saving historical agents.
+  + each_episode_rewards_history.csv
+    > Detailed data of historical reward.
+  + each_FanPower_history.csv
+    > Detailed data of historical energy consumption of constant humidity machine
+  + episode_reward_history.csv
+    > Historical reward.
+  + FanPower_history.csv
+    > Historical energy consumption of constant humidity machine.
+  + Loss_history.csv
+    > Historical data of loss value.
+  + mypython.out
+    > Output information of the console.
 
--- pid
-+ PID algorithm.
+## Data description ##
 
--- solutions
-+ The module related to reinforcement learning and the module interacting with the model of STAR-CCM+.
+Data column description of 'csvfile' directory.
+
+- "Physical Time: Physical Time (s)"
+    > Simulation physical time.
+- "InletDoor Monitor: Surface Average of RH"
+    > Humidity at gate entrance.
+- "InletRHa Monitor: Surface Average of RH","InletRHb Monitor: Surface Average of RH"
+,"InletRHc Monitor: Surface Average of RH"
+    > Humidity of air outlet of constant humidity machine a, b, c.
+- "InletVelDoor Monitor: Surface Average of Velocity: Magnitude (m/s)"
+,"InletVela Monitor: Surface Average of Velocity: Magnitude (m/s)"
+,"InletVelb Monitor: Surface Average of Velocity: Magnitude (m/s)"
+,"InletVelc Monitor: Surface Average of Velocity: Magnitude (m/s)"
+    > Wind speed at the gate entrance and the outlets of a, b, c constant humidity machine.
+- "点10RH Monitor: Sum of RH","点2RH Monitor: Sum of RH",......,"点21RH Monitor: Sum of RH"
+    > Humidity of 21 sensors in the room.
 
 
 ---------------
@@ -123,25 +184,84 @@ HumidityControl
 
 ## 文件目录 ##
 
-HumidityControl
+- HumidityControl
+  + DifferentArithmetic
+    >DQN、Rainbow、和Rainbow消融算法的模型训练
+  + DifferentSetpoints
+    > 模型A的不同监测点(6、9、15个湿度传感器)实验
+  + DifferentTime
+    > 模型A的不同上报时间间隔(1、3、5分钟)实验
+  + modelAtest
+    > 测试不同算法在模型A的效果
+  + modelB
+    > 测试不同算法在模型B的效果
+  + pid
+    > PID算法
+  + solutions
+    > 强化学习相关的模块,与STAR-CCM+的模型进行交互.
 
--- DifferentArithmetic
-+ DQN、Rainbow、和Rainbow消融算法的模型训练
+## 文件详细说明 ##
 
--- DifferentSetpoints
-+ 模型A的不同监测点(6、9、15个湿度传感器)实验
+此处只说明rainbow算法目录下的数据和文件说明，其他算法的文件存放方式与其一致。
 
--- DifferentTime
-+ 模型A的不同上报时间间隔(1、3、5分钟)实验
+rainbow目录的具体位置：
 
--- modelAtest
-+ 测试不同算法在模型A的效果
+- HumidityControl
+  + DifferentArithmetic
+    + rainbow
 
--- modelB
-+ 测试不同算法在模型B的效果
+rainbow文件夹下的说明：
 
--- pid
-+ PID算法
+- javafile 
+  > 存放仿真模型和宏命令
+  + checkinfo.java 
+    > 检查info.txt文件操作. 
+  + editandrun.java 
+    > 修改CFD模型相关的边界条件，比如修改恒湿机的温度、湿度、风速.
+  + info.txt 
+    > 时间校验文件，维持仿真模型和agent的物理时间一致性.
+  + reset.java 
+    > 重置仿真环境的宏命令.
+  + RHfile.csv 
+    > 当前episode的交互数据
+  + humidity.sim 
+    > CFD仿真模型.
+- out 
+  > 存放历史文件（模型的数据和agent的策略）
+  + csvfile 
+    >记录历史每个episode的交互数据
+  + logs 
+    >记录控制台的相关信息
+  + savemodel 
+    >保存历史智能体的target网络
+  + each_episode_rewards_history.csv
+    > 历史reward详细数据
+  + each_FanPower_history.csv
+    > 恒湿机的历史能耗详细数据
+  + episode_reward_history.csv
+    > 历史reward
+  + FanPower_history.csv
+    > 恒湿机的历史能耗
+  + Loss_history.csv
+    > loss值的历史数据
+  + mypython.out
+    > 控制台的输出信息
 
--- solutions
-+ 强化学习相关的模块,与STAR-CCM+的模型进行交互.
+## 数据说明 ##
+
+csvfile目录的数据列说明
+
+- "Physical Time: Physical Time (s)"
+    > 仿真物理时间
+- "InletDoor Monitor: Surface Average of RH"
+    > 大门入口的湿度
+- "InletRHa Monitor: Surface Average of RH","InletRHb Monitor: Surface Average of RH"
+,"InletRHc Monitor: Surface Average of RH"
+    > 恒湿机a,b,c的出风口的湿度
+- "InletVelDoor Monitor: Surface Average of Velocity: Magnitude (m/s)"
+,"InletVela Monitor: Surface Average of Velocity: Magnitude (m/s)"
+,"InletVelb Monitor: Surface Average of Velocity: Magnitude (m/s)"
+,"InletVelc Monitor: Surface Average of Velocity: Magnitude (m/s)"
+    > 分别是大门入口、恒湿机a,b,c出风口的风速
+- "点10RH Monitor: Sum of RH","点2RH Monitor: Sum of RH",......,"点21RH Monitor: Sum of RH"
+    > 房间内传感器的湿度（21个）
